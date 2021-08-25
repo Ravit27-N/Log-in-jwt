bd8a8c28837cf845ee38d7b3798c9380245a2e2c
RavitHun <77621123+Ravit27-N@users.noreply.github.com>
2021-08-25 09:38:06 +0700

    update database

--- utils/Database_TBL.sql
+++ utils/Database_TBL.sql
@@ -11,14 +11,14 @@ CREATE TABLE users (
  ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE user ( 
-'id' INT NOT NULL AUTO_INCREMENT , 
-'first_name' VARCHAR(45) NOT NULL , 
-'last_name' VARCHAR(45) NOT NULL , 
-'email' VARCHAR(45) NOT NULL , 
-'phone' VARCHAR(45) NOT NULL , 
-'comments' TEXT NOT NULL , 
-'status' VARCHAR(10) NOT NULL DEFAULT 'active' ,
+CREATE TABLE `user` ( 
+`id` INT NOT NULL AUTO_INCREMENT , 
+`first_name` VARCHAR(45) NOT NULL , 
+`last_name` VARCHAR(45) NOT NULL , 
+`email` VARCHAR(45) NOT NULL , 
+`phone` VARCHAR(45) NOT NULL , 
+`comments` TEXT NOT NULL , 
+`status` VARCHAR(10) NOT NULL DEFAULT 'active' ,
  PRIMARY KEY (`id`));
  
  INSERT INTO `user` 
